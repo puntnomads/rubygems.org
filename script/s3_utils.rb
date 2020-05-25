66267df068b8e6230e2af17e59c9e15c191ed2d8
puntnomads <puntnomads@gmail.com>
2020-05-25 18:46:18 +0100

    Remove unreachable code: md5_compare_s3_versions
    The relevant commits are faaf4203f26460c9bf1c3a497228ee9e55c79e90 and 83f0dd17321d2d341a81be7a3c957152e4440f7d.

--- script/s3_utils.rb
+++ script/s3_utils.rb
@@ -17,12 +17,6 @@ class S3Utils
     deltas.sort_by { |d, _| -d }
   end
 
-  def md5_compare_s3_versions(version_full_name)
-    @s3.list_object_versions(bucket: @bucket, prefix: "gems/#{version_full_name}.gem").versions.map do |vs3|
-      body = @s3.get_object(key:  vs3.key, bucket: @bucket, version_id: vs3.version_id).body.read
-      Digest::MD5.hexdigest(body)
-    end
-  end
 
   def write_s3_versions(version_full_name)
     @s3.list_object_versions(bucket: @bucket, prefix: "gems/#{version_full_name}.gem").versions.map do |vs3|
